883649654547768eed42c2eb0bfcbb718088da51
ibrokhim-command <ibrokhimofficial@naver.com>
2020-02-14 23:23:58 +0900

    /js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function(request,respons){
+  respons.sendFile(__dirname + '/index.html');
+});
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', function(socket){
+  console.log('Ulanish muvaffaqiyatli amalga oshdi.');
+  connections.push(socket);
+
+  socket.on('disconnect', function(data){
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Tarmoqdan uzildingiz.');
+  });
+
+  socket.on('send mess', function(data){
+    io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+  });
+});
